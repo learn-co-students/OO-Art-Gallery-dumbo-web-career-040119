02d827f7c0af3694a40f0263fc52877dc5d57ef1
github username <github email address>
2019-04-09 16:30:10 -0400

    finished

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,49 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+    paintings = Painting.all.select do |painting|
+      painting.gallery == self
+    end
+    #binding.pry
+  end
+
+  def artists
+    artists = self.paintings.map do |paintings|
+      paintings.owner
+    end
+  end
+
+  def artist_names
+
+    artist_names = self.artists.map do |artists|
+      artists.name      
+    end
+
+  end
+
+  def most_expensive_painting
+    empty_arr = []
+    expensive_price = 0
+    self.paintings.map do |paintings|
+      #binding.pry
+      if paintings.price > expensive_price
+        expensive_price = paintings.price
+        empty_arr = paintings
+      end
+    end
+    empty_arr
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :owner
 
   @@all = []
+  @@total_price = 0
 
-  def initialize(title, price)
+  def initialize(title, price,gallery,owner)
     @title = title
     @price = price
+    @gallery = gallery
+    @owner = owner
+    @@all << self
+  end
+
+  def self.total_price
+    @@total_price 
+  end
+
+  def self.all
+    @@all
   end
 
 end
