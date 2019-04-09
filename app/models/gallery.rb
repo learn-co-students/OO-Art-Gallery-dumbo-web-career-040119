fc698bfe059d10b7fa93be33e20242d47fabaf05
Mallory Woods <malvwoods@gmail.com>
2019-04-09 14:44:38 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,37 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+#DONE * `Gallery.all` * Returns an `array` of all the galleries
+  def self.all
+    @@all
+  end
+
+#DONE * `Gallery#paintings` * Returns an `array` of all paintings in a gallery
+  def paintings
+    Painting.all.select { |painting| painting.gallery == self}
+  end
+
+#DONE * `Gallery#artists` * Returns an `array` of all artists that have a painting in a gallery
+  def artists
+      self.paintings.map(&:artist)
+  end
+
+#DONE * `Gallery#artist_names` * Returns an `array` of the names of all artists that have a painting in a gallery
+  def artist_names
+    self.artists.map(&:name)
+  end
+
+
+#DONE * `Gallery#most_expensive_painting` * Returns an `instance` of the most expensive painting in a gallery
+  def most_expensive_painting
+    self.paintings.max_by(&:price)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
+require 'pry'
+
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
+  end
+
+#DONE * `Painting.all`
+#* Returns an `array` of all the paintings
+  def self.all
+    @@all
+  end
+
+#DONE * `Painting.total_price`
+#   * Returns an `integer` that is the total price of all paintings
+  def self.total_price
+    total = self.all.map(&:price).inject(0, &:+)
   end
 
 end
