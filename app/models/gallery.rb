4673e0ad5dc52cd829241875a50c8d08a9a2c9aa
cogilvy <chrisogilvy@cox.net>
2019-04-09 18:44:36 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,45 @@
 class Gallery
-
   attr_reader :name, :city
+
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  # Returns an array of all the galleries
+  def self.all
+    @@all
+  end
+
+  # Returns an array of all paintings in a gallery
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+  # Returns an array of all artists that have a painting in a gallery.
+  def artists
+    paintings.map do |painting|
+      painting.artist
+    end
+  end
+
+  # Returns an array of the names of all artists that have a painting in a gallery
+  def artist_names
+    artists.map do |artist|
+      artist.name
+    end
+  end
+
+  # Returns an instance of the most expensive painting in a gallery.
+  def most_expensive_painting
+    paintings.max_by do |painting|
+      painting.price
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,61 @@
 require_relative '../config/environment.rb'
 
+# Create some new artists
+# initialize(name, years_experience)
+davinci = Artist.new("Leo", 40)
+michaelangelo = Artist.new("Micky", 25)
+chris = Artist.new("Chris", 3)
+dave = Artist.new("Dave", 7)
 
+# Create some galleries
+# initialize(name, city)
+new_york = Gallery.new("New York Metro", "New York")
+la = Gallery.new("Los Angeles Museum", "Los Angeles")
 
+# LASTLY Create some paintings. Do this last because we will need to input a
+# value for artist and gallery in painting.
+# initialize(title, price, gallery, artist)
+painting1 = Painting.new("The Best Painting Ever", 10_000, new_york, chris)
+painting2 = Painting.new("Mona Lisa", 300_000, new_york, davinci)
+painting3 = Painting.new("Angels Touching Hands", 1_000_000, la, michaelangelo)
+painting4 = Painting.new("Scream", 450_000, la, dave)
 
 binding.pry
+# Now test all the methods you can call on each class!
+# Plug in the following methods in binding.pry to test!
+#################################################################
+# Artist Methods
 
-puts "Bob Ross rules."
+p Artist.all
+
+p davinci.paintings
+
+p michaelangelo.galleries
+
+p dave.cities
+
+p Artist.total_experience
+
+p Artist.most_prolific
+
+p chris.create_painting("Test Title", 10, new_york)
+
+#################################################################
+# Painting Methods
+
+p Painting.all
+
+p Painting.total_price
+
+#################################################################
+# Gallery Methods
+
+p Gallery.all
+
+p new_york.paintings
+
+p la.artists
+
+p la.artist_names
+
+p new_york.most_expensive_painting
