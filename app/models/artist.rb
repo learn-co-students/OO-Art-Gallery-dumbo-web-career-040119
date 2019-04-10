a20092f37be0e96a6333093a19062b84e4ec6d54
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-09 23:17:20 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,48 @@
 class Artist
 
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.map(&:gallery)
+  end
+
+  def galleries
+    self.paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    self.galleries.map(&:city)
+  end
+
+  def self.total_experience
+    self.all.map(&:years_experience).reduce{|sum,e|sum+=e}
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      (artist.paintings.length / artist.years_experience)
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    new_paint = Painting.new(title, price, self, gallery)
+  end
+
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,34 @@
 class Gallery
 
   attr_reader :name, :city
-
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    # Painting.all.map(&:gallery).select {|gallery| gallery == self}
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    # Painting.all.map(&:artist)
+    # Painting.all.select{|paint| paint.gallery == self}.map(&:artist)
+    self.paintings.map(&:artist)
+  end
+
+  def artist_names
+    self.artists.map(&:name)
+  end
+
+  def most_expensive_painting
+    self.paintings.max_by { |paint| paint.price }
   end
 
 end
