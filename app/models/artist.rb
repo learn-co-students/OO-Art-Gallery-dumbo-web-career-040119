e2bed48a3fb8299b0fb247c35d8cf3f060352db3
ftse105 <ftse105@gmail.com>
2019-04-10 08:43:47 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,48 @@
+require 'pry'
+
 class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+#Returns an `array` of all the artists
+  def self.all
+    @@all
+  end
+
+#Returns an `array` all the paintings by an artist
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+#Returns an `array` of all the galleries that an artist has paintings in
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+  #Return an `array` of all cities that an artist has paintings in
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+  #Returns an `integer` that is the total years of experience of all artists
+  def self.total_experience
+    self.all.reduce(0) do |sum, artist|
+      sum + artist.years_experience
+    end
+  end
+#Given the arguments of `title`, `price` and `gallery`, creates a new painting belonging to that artist
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,18 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("John", 15)
+a2 = Artist.new("Jose", 6)
+a3 = Artist.new("Jesus", 70)
 
 
+g1 = Gallery.new("Modern", "Brooklyn")
+g2 = Gallery.new("Present", "Dumbo")
+g3 = Gallery.new("Future", "Manhattan")
+
+
+p1 = Painting.new("random", "$999", a1, g1)
+p2 = Painting.new("anything", "$777", a2, g2)
+p3 = Painting.new("where", "$650", a3, g1)
 
 binding.pry
 
